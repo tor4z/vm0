33fa87b1f8b00d96bf3b506e1c6fb34a595506cb
tor4z <vwenjie@hotmail.com>
2022-09-04 01:14:41 +0800

    Add processor crate

---
+++ src/processor.rs
@@ -0,0 +1,18 @@
+pub struct Processor {
+
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::Processor;
+
+    #[test]
+    #[allow(unused_variables)]
+    fn test_new_processor() {
+        let proc = Processor{};
+
+    }
+
+}
