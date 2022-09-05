a7130b11e04e49629974542523bdb22ac87a6a7c
tor4z <vwenjie@hotmail.com>
2022-09-06 00:50:48 +0800

    Add write memory with vector method

--- src/memory.rs
+++ src/memory.rs
@@ -31,6 +31,17 @@ impl Memory {
     pub fn new(cap: usize) -> Self {
         Memory{cap: cap, data: vec![0u8; cap]}
     }
+
+    pub fn write(&mut self, data: &Vec<u8>, mut index: usize) -> Result<(), &str> {
+        for v in data.iter() {
+            self.data[index] = *v;
+            index += 1;
+            if index >= self.cap {
+                panic!("Memory out of range")
+            }
+        }
+        Ok(())
+    }
 }
 
 
@@ -65,5 +76,23 @@ mod tests {
         let mut mem = Memory::new(1024);
         mem[0] = 0x1;
         assert_eq!(mem[0], 0x01);
+    }
+
+    #[test]
+    fn test_memory_write_mem_from_vec() {
+        let mut mem = Memory::new(1024);
+        let data : Vec<u8> = vec![0, 1, 2];
+        mem.write(&data, 0).unwrap();
+        for i in 0..2 {
+            assert_eq!(mem[i], i as u8)
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_memory_write_mem_from_vec_overflow() {
+        let mut mem = Memory::new(64);
+        let data : Vec<u8> = vec![1; 100];
+        mem.write(&data, 0).unwrap();
     }
 }
